178fd4963dab5111c2e547e90efb387be0e6041f
pallao23 <palla02323@gmail.com>
2022-09-16 15:52:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react';
+import Questions from './questions.json';
+import Display from './Display';
+import Model from './Model'
+
+const App = () => {
+  const [marks,setMarks] = useState([]);
+  let tempMarks = [];
+  
+  const handleChange = (index,mark) =>{
+    tempMarks = marks
+    tempMarks[index] = mark;
+    setMarks([...tempMarks]);
+   // alert(tempMarks);
+  }
+
+  return (
+    <div>
+      <center>
+        <h1>Quiz Application</h1> 
+              {/* First we have imported Json Data and then Hydrate and then send to another components or 
+              return (<h2>{qa.tile})</h2> in same component or
+              console log the data*/}
+        {Questions.map((qa,index) => <Display question={qa} handleChange={handleChange} index={index} />)}
+        <Model marks={marks}/>
+      </center>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Display.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Qa = ({ question, handleChange, index }) => {
+    const handler = (e) => {
+        let temp = question.answer === e.target.value ? 1 : 0;
+        handleChange(index, temp);
+    }
+    return (
+        <div className='container'>
+            <div style={{ "textAlign": "left", "border": "1px solid black", "margin": "4px", "padding": "1px" }} >
+                <div className="card  ">
+                    <div className="card-body ">
+                        <h5 className="card-title">
+                            {question.title}
+                        </h5>
+                        {/* All the input fields are belonging to one Question Only, for that we Have Used name var.
+                            unique name for each and every Question options for that we have used index position 
+                            For Each Question ,Index Postion Will Be Changed    name={index} */}
+                        <div onChange={handler}>
+                            <input type="radio" value="A" name={index} /> <span>{question.options[0]}</span> &nbsp;
+                            <input type="radio" value="B" name={index} /> <span>{question.options[1]}</span> &nbsp;
+                            <input type="radio" value="C" name={index} /> <span>{question.options[2]}</span> &nbsp;
+                            <input type="radio" value="D" name={index} /> <span>{question.options[3]}</span> &nbsp;
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Qa
