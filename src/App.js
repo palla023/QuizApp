6f38b4dda4541b24570a4947cf7fe119cab23a0f
palla023 <65664262+palla023@users.noreply.github.com>
2024-08-05 03:18:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Model from './Model'
 const App = () => {
   const [marks,setMarks] = useState([]);
   let tempMarks = [];
-  
+
   const handleChange = (index,mark) =>{
     tempMarks = marks
     tempMarks[index] = mark;
@@ -18,7 +18,7 @@ const App = () => {
     <div>
       <center>
         <h1>Quiz Application</h1> 
-              {/* First we have imported Json Data and then Hydrate and then send to another components or 
+        {/* First we have imported Json Data and then Hydrate and then send to another components or 
               return (<h2>{qa.tile})</h2> in same component or
               console log the data*/}
         {Questions.map((qa,index) => <Display question={qa} handleChange={handleChange} index={index} />)}
