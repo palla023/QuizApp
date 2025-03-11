c4a29b0d80f34bc3349449dbeb482f1d3cb30223
palla023 <65664262+palla023@users.noreply.github.com>
2025-03-11 10:14:13 +0530

    Update Model.js with popUp

--- src/Model.js
+++ src/Model.js
@@ -1,32 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Sub = ({marks}) => {
+const Sub = ({ marks }) => {
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <div>
-           <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">End Quiz</button>
+            {/* Button to Show Modal */}
+            <button 
+                type="button" 
+                className="btn btn-info btn-lg" 
+                onClick={() => setShowModal(true)}
+            >
+                End Quiz
+            </button>
 
-            <div class="modal fade" id="myModal" role="dialog">
-            <div class="modal-dialog">
-            
-                <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Your Score :</h4>
+            {/* Modal */}
+            {showModal && (
+                <div className="modal fade show" style={{ display: 'block', background: 'rgba(0,0,0,0.5)' }}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close" onClick={() => setShowModal(false)}>
+                                    &times;
+                                </button>
+                                <h4 className="modal-title">Your Score:</h4>
+                            </div>
+                            <div className="modal-body">
+                                <h1 style={{ textAlign: "center" }}>
+                                    {marks.reduce((a, b) => a + b, 0)}/10
+                                </h1>
+                            </div>
+                            <div className="modal-footer">
+                                <button 
+                                    type="button" 
+                                    className="btn btn-default" 
+                                    onClick={() => setShowModal(false)}
+                                >
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="modal-body">
-                    <h1 style={{"textAlign" : "center"}}>{marks && marks.reduce((a,b) => a+(b || 0),0)}/10</h1>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-                </div>
-                
-            </div>
-            </div>
-
-            <br/>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default Sub
+export default Sub;
